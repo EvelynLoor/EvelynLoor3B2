79ea7560cc9fac6fec583e13ac596824d0e73c58
EvelynLoor <118677416+EvelynLoor@users.noreply.github.com>
2022-11-20 13:13:22 -0500

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,48 @@
+//Se pretende realizar una aplicación para esta facultad que gestione la información 
+//sobre las personas vinculadas con la misma, 
+//que se pueden clasificar en tres tipos: 
+//estudiantes, profesores y personal de servicio. 
+//•	Por cada persona, se debe conocer, al menos, su nombre y apellidos, 
+//su número de identificación y su estado civil. 
+
+
+class program{
+
+public string nombre;
+public int cedula;
+public int estadoCivil;
+{
+
+public string nombre e1= new nombre();
+e1.EscribeNombre("Karen");
+public int cedula c1= new cedula();
+c1.EscribeCedula("1313511295");
+public int estadoCivil ec1=new estadoCivil();
+ec1.EscribeCivil("Divorciada");
+
+public string nombre p2 =new nombre();
+p2.EscribeNombre("Wendy");
+public int cedula c2=new cedula();
+c2.EscribeCedula("1316451341");
+public int estadoCivil ec2=new estadoCivil();
+ec2.EscribeCivil("Casada");
+
+
+public string nombre ps3=new nombre();
+ps3.EscribeNombre("Nallely");
+public int cedula c3=new cedula();
+c3.EscribeCedula("1316451358");
+public int estadoCivil ec3=new estadoCivil;
+ec3.EscribeCivil("Soltera");
+public int año a1=new año();
+a1.Escribeaño("2017");
+public int despacho d1=new despacho();
+d1.Escribedespacho("#5");
+public string seccion s1=new seccion();
+s1.EscribeSeccion("Secretaria");
+}
+
+
+}
+
+
